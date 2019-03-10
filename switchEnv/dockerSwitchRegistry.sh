601eeb9d30189ff15e36b8197112355c7d130f4c
Ethan <940941188@qq.com>
2019-03-10 18:56:32 +0800

    ADD docker switch registry

---
+++ switchEnv/dockerSwitchRegistry.sh
@@ -0,0 +1,24 @@
+method="${1}";
+case ${method} in
+  -o) 
+    sudo mkdir -p /etc/docker;
+    # sudo tee /etc/docker/daemon.json << 'EOF'{}EOF
+    sudo systemctl daemon-reload
+    sudo systemctl restart docker
+    ;;
+  -a) 
+    sudo mkdir -p /etc/docker;
+    # sudo tee /etc/docker/daemon.json << 'EOF'
+    # {
+    #   "registry-mirrors": ["https://yourcode.mirror.aliyuncs.com"]
+    # }
+    # EOF
+
+    sudo systemctl daemon-reload
+    sudo systemctl restart docker
+    ;;
+  *)
+    echo "`basename ${0}`:usage: [-o origion] | [-a aliyuncs mirror ]"
+    exit 1 # Command to come out of the program with status 1
+    ;;
+esac
